20bb22038ea54d89792c9a40990fe01e225c7d0c
Justin Yu <justin.v.yu@gmail.com>
2015-10-10 14:19:31 -0700

    Add append file

--- client/views/add/add.js
+++ client/views/add/add.js
@@ -1,6 +1,37 @@
+var numFiles = [1];
+
 Controller('add', {
 
+    helpers: {
+        numFiles: numFiles
+    },
+
+    events: {
+        'click #add-md': function() {
+            // numFiles.push(numFiles[numFiles.length - 1] + 1);
+            $("#md-files").append("<div class='field'><label>Section Title</label><input type='text' placeholder='Section Title'><input type='file'></div>");
+        },
+        'submit form': function(event) {
+            event.preventDefault();
+
+            var title = $("#title-input").val();
+            var author = $("#author-input").val();
+            var language = $("#language-input").val();
+            console.log(title, author, language);
+
+            if (title == "" || author == "" || language == "") {
+                return;
+            }
+
+            Workshops.insert({
+                title: title,
+                author: author,
+                language: language
+            });
+        }
+    },
+
     rendered: function() {
-        
+
     }
 });
